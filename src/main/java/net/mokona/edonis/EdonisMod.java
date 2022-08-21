a1b60adbce8f4aebeb8ca71204d9a9314fbfbd27
MokonaNico <mine-nico@hotmail.com>
2022-08-21 22:11:12 +0200

    add first item

--- src/main/java/net/mokona/edonis/EdonisMod.java
+++ src/main/java/net/mokona/edonis/EdonisMod.java
@@ -1,18 +1,13 @@
 package net.mokona.edonis;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.mokona.edonis.item.ModItems;
 import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
@@ -27,7 +22,9 @@ public class EdonisMod {
 
     public EdonisMod() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(eventBus);
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
